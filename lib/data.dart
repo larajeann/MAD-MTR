fdfd4679239bdbd2bcfa327681adce7dcae856c0
larajeann <larajeanalvaro@gmail.com>
2024-03-28 00:24:46 +0800

    assets-added

--- lib/data.dart
+++ lib/data.dart
@@ -26,17 +26,59 @@ List<String> fillSourceArray() {
   ];
 }
 
+List<String> fillSourceArrayM() {
+  return [
+    'assets/animalspics/dingdong.png',
+    'assets/animalspics/dingdong.png',
+    'assets/animalspics/leah.png',
+    'assets/animalspics/leah.png',
+    'assets/animalspics/luna.png',
+    'assets/animalspics/luna.png',
+    'assets/animalspics/rizal.png',
+    'assets/animalspics/rizal.png',
+    'assets/animalspics/wang.png',
+    'assets/animalspics/wang.png',
+    'assets/animalspics/manny.png',
+    'assets/animalspics/manny.png',
+  ];
+}
+List<String> fillSourceArrayH() {
+  return [
+    'assets/animalspics/1.png',
+    'assets/animalspics/1.png',
+    'assets/animalspics/2.png',
+    'assets/animalspics/2.png',
+    'assets/animalspics/3.png',
+    'assets/animalspics/3.png',
+    'assets/animalspics/4.png',
+    'assets/animalspics/4.png',
+    'assets/animalspics/5.png',
+    'assets/animalspics/5.png',
+    'assets/animalspics/6.png',
+    'assets/animalspics/6.png',
+    'assets/animalspics/7.png',
+    'assets/animalspics/7.png',
+    'assets/animalspics/8.png',
+    'assets/animalspics/8.png',
+    'assets/animalspics/9.png',
+    'assets/animalspics/9.png',
+  ];
+}
 List<String> getSourceArray(
   Level level,
 ) {
   List<String> levelAndKindList = <String>[]; 
   List<String> sourceArray = fillSourceArray().cast<String>();
+  List<String> sourceArrayM = fillSourceArrayM().cast<String>();
+  List<String> sourceArrayH = fillSourceArrayH().cast<String>();
   if (level == Level.Hard) {
-    levelAndKindList.addAll(sourceArray);
+    for (int i = 0; i < 18; i++) {
+      levelAndKindList.add(sourceArrayH[i]);
+    }
   } 
   else if (level == Level.Medium) {
     for (int i = 0; i < 12; i++) {
-      levelAndKindList.add(sourceArray[i]);
+      levelAndKindList.add(sourceArrayM[i]);
     }
   } else if (level == Level.Easy) {
     for (int i = 0; i < 6; i++) {
